9e0840e56bba41b8e52f188e25533594cc0c0f57
Jim Nasby <Jim.Nasby@BlueTreble.com>
2016-11-05 18:02:19 -0500

    add -d support for < 9.2

--- load.sh
+++ load.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+version=`psql -X -qtc "SELECT current_setting('server_version_num')"` || exit $?
+
 if [ "$1" == "-d" ]; then
     shift
-    dropdb --if-exists $@ || exit $?
+    if [ $version -ge 90200 ]; then
+        dropdb --if-exists $@ || exit $?
+    else
+        psql -c 'SELECT 1' $@ >/dev/null 2>&1 && (dropdb $@ || exit $?)
+    fi
 fi
 
 createdb $@ || exit $?
@@ -12,7 +18,6 @@ cd `dirname $0` || exit $?
 psql -X -v ON_ERROR_STOP=1 -c 'CREATE EXTENSION IF NOT EXISTS cat_tools;' $@ || exit $?
 psql -X -v ON_ERROR_STOP=1 -f catalog.sql $@ || exit $?
 
-version=`psql -X -qtc "SELECT current_setting('server_version_num')"` || exit $?
 if [ $version -ge 90400 ]; then
     psql -X -v ON_ERROR_STOP=1 -f views/expanded.sql $@ || exit $?
 else
